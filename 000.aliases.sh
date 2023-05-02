7092274461db3b9bd0a3202f1021b739d3c8e340
optimizacija <males.jan2@gmail.com>
2023-05-02 13:53:10 +0200

    feat: added rgm

--- 000.aliases.sh
+++ 000.aliases.sh
@@ -5,6 +5,7 @@ alias v=nvim
 alias rg='rg --auto-hybrid-regex'
 alias jqfmt='jq -R -r '\''. as $raw | try fromjson catch $raw'\'''
 alias tolower='awk '\''{print tolower($0)}'\'''
+alias rgm='rg --multiline --multiline-dotall'
 
 # default editor = nvim
 export EDITOR=nvim
