cb215d676a0a076106dffa22d84f82688e404b90
optimizacija <males.jan2@gmail.com>
2023-05-30 16:15:04 +0200

    feat: updated jqfmt to fix stacktracing

--- 000.aliases.sh
+++ 000.aliases.sh
@@ -3,7 +3,9 @@ alias q=exit
 alias vim=nvim
 alias v=nvim
 alias rg='rg --auto-hybrid-regex'
-alias jqfmt='jq -R -r '\''. as $raw | try fromjson catch $raw'\'''
+# alias jqfmt='jq -R -r '\''. as $raw | try fromjson catch $raw'\'''
+alias jqfmt='jq -R -r '\''. as $raw | try (fromjson | if has("stacktrace") then .stacktrace |= split("\n") else . end) catch $raw'\'''
+
 alias tolower='awk '\''{print tolower($0)}'\'''
 alias rgm='rg --multiline --multiline-dotall'
 
