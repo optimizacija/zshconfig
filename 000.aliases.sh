9550a1540c44c3701fb49d19624eb706abc943b4
optimizacija <males.jan2@gmail.com>
2023-08-11 15:14:49 +0200

    feat: added alias & brew link# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch main# Your branch is ahead of 'origin/main' by 2 commits.#   (use "git push" to publish your local commits)## Changes to be committed:#	modified:   000.aliases.sh#	new file:   000.links.sh## ------------------------ >8 ------------------------# Do not modify or remove the line above.# Everything below it will be ignored.diff --git a/000.aliases.sh b/000.aliases.shindex 0635964..4066a82 100644--- a/000.aliases.sh+++ b/000.aliases.sh@@ -4,7 +4,8 @@ alias vim=nvim alias v=nvim alias rg='rg --auto-hybrid-regex' # alias jqfmt='jq -R -r '\''. as $raw | try fromjson catch $raw'\'''-alias jqfmt='jq -R -r '\''. as $raw | try (fromjson | if has("stacktrace") then .stacktrace |= split("\n") else . end) catch $raw'\'''+# alias jqfmt='jq -R -r '\''. as $raw | try (fromjson | if has("stacktrace") then .stacktrace |= split("\n") else . end) catch $raw'\'''+alias jqfmt='jq -R -r '\''. as $raw | try (fromjson | if has("stacktrace") then .stacktrace |= (split("\n") | map(gsub("\t"; ""))) else . end) catch $raw'\'''  alias tolower='awk '\''{print tolower($0)}'\''' alias rgm='rg --multiline --multiline-dotall'diff --git a/000.links.sh b/000.links.shnew file mode 100644index 0000000..d7c1194--- /dev/null+++ b/000.links.sh@@ -0,0 +1 @@+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"

--- 000.aliases.sh
+++ 000.aliases.sh
@@ -4,7 +4,8 @@ alias vim=nvim
 alias v=nvim
 alias rg='rg --auto-hybrid-regex'
 # alias jqfmt='jq -R -r '\''. as $raw | try fromjson catch $raw'\'''
-alias jqfmt='jq -R -r '\''. as $raw | try (fromjson | if has("stacktrace") then .stacktrace |= split("\n") else . end) catch $raw'\'''
+# alias jqfmt='jq -R -r '\''. as $raw | try (fromjson | if has("stacktrace") then .stacktrace |= split("\n") else . end) catch $raw'\'''
+alias jqfmt='jq -R -r '\''. as $raw | try (fromjson | if has("stacktrace") then .stacktrace |= (split("\n") | map(gsub("\t"; ""))) else . end) catch $raw'\'''
 
 alias tolower='awk '\''{print tolower($0)}'\'''
 alias rgm='rg --multiline --multiline-dotall'
