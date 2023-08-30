310e76e3902e2f4b58bf16c99f7c21119215f252
optimizacija <males.jan2@gmail.com>
2023-08-30 16:04:10 +0200

    feat: added veza stuff

--- 400.veza.sh
+++ 400.veza.sh
@@ -3,6 +3,8 @@ alias cdc='cd /Users/Shared/dev/src/github.com/cookieai-inc/cookieai-core'
 export CDPATH='/Users/Shared/dev/src/github.com/cookieai-inc/cookieai-core'
 alias bazel_run_record='bazel run --config=record'
 alias bazel_run_record_skip_extraction='bazel run --config=record --define=skip_test_extraction=true'
+alias bazel_run_lint='bazel run //tools:lint go'
+alias bazel_gen_proto='bazel run //tools:genproto'
 
 function clean_docker() {
     cd /Users/Shared/dev/src/github.com/cookieai-inc/cookieai-core
@@ -16,4 +18,8 @@ function genreachability_fingerprint {
         -output-reachability-fingerprint `realpath controlp/internal/graph/precomputed_reachability_fingerprint.txt` \
         -output-reachability `realpath controlp/internal/graph/precomputed_reachability_map.go` \
         -output-graphplanner `realpath controlp/internal/graph/graphplanner`
+}
+
+function upgrade_tenant {
+   bazel run //tools/cmd/kubecreate -- tenant upgrade -n tenant1 
 }
