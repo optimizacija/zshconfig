066d9ed26546a801be67292ba9d9c31585b14ca1
optimizacija <males.jan2@gmail.com>
2023-08-30 15:56:41 +0200

    feat(veza): added genreachability_fingerprint && bazel run aliases

--- 400.veza.sh
+++ 400.veza.sh
@@ -1,10 +1,19 @@
 #!/bin/zsh
 alias cdc='cd /Users/Shared/dev/src/github.com/cookieai-inc/cookieai-core'
 export CDPATH='/Users/Shared/dev/src/github.com/cookieai-inc/cookieai-core'
+alias bazel_run_record='bazel run --config=record'
+alias bazel_run_record_skip_extraction='bazel run --config=record --define=skip_test_extraction=true'
 
 function clean_docker() {
     cd /Users/Shared/dev/src/github.com/cookieai-inc/cookieai-core
     bazel run //tools/cmd/dockerclean
     scripts/devenv/start_docker_registry.sh
     bazel run //tools/cmd/kubecreate -- up -t --debug
+}
+
+function genreachability_fingerprint {
+    bazel run //controlp/internal/graph/cmd/genreachability -- \
+        -output-reachability-fingerprint `realpath controlp/internal/graph/precomputed_reachability_fingerprint.txt` \
+        -output-reachability `realpath controlp/internal/graph/precomputed_reachability_map.go` \
+        -output-graphplanner `realpath controlp/internal/graph/graphplanner`
 }
